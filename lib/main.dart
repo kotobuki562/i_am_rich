73579aef25b7bd6e8f11257c7f56a0e76acdf6fa
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-02-01 12:12:12 +0900

    addImage

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
